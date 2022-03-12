fa5599b13221bfe456d3b712e3a9dd0da8b3bbc4
karim076 <59794989+karim076@users.noreply.github.com>
2022-03-12 14:57:31 +0100

    function: new table with information and changeble

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 
 <head>
     <title>Prikbord</title>
+    <link rel="stylesheet" href="css/main.css">
     <?php require_once 'head.php'; ?>
 </head>
 
@@ -26,9 +27,24 @@
             <!-- Schrijf hier een foreach die door $berichten loopt -->
             <li>Hier komt een lijst met berichten.</li>
         </ul>
-
+        <table>
+            <tr>
+                <th>Titel</th>
+                <th>Bericht</th>
+                <th>Schrijver</th>
+                <th>Aanpassen</th>
+            </tr>
+            <?php $number = 0; ?>
+            <?php foreach($berichten as $melding): ?>
+                <tr>
+                    <?php $number += 1; ?>
+                    <td><?php echo $melding['title']; ?></td>
+                    <td><?php echo $melding['content']; ?></td>
+                    <td><?php echo $melding['author']; ?></td>
+                    <td><a href="berichten/edit.php?id=<?php echo $number;?>">Aanpassen</a></td>
+                </tr>
+            <?php endforeach; ?>
+        </table>
     </div>
-
 </body>
-
 </html>
