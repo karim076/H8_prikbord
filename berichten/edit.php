5ea6cf8194c7d5101deddbc33411899e969fd7bd
karim076 <59794989+karim076@users.noreply.github.com>
2022-03-12 14:58:47 +0100

    Function: made connection with server - finished the table to change the information in the database

--- berichten/edit.php
+++ berichten/edit.php
@@ -21,13 +21,23 @@
         require_once '../backend/conn.php';
         $query = "SELECT * FROM berichten WHERE id = :id";
         //(maak het 5-stappenplan nog af)
-
+        $statement = $conn->prepare($query);
+        $statement->execute([":id" => $id]);
+        $item = $statement->fetch(PDO::FETCH_ASSOC);
         ?>
 
         <!-- Formulier voor edit: -->
-        <form action="..." method="POST">
+        <form action="../backend/berichtenController.php" method="POST">
             <input type="hidden" name="action" value="edit">
             <input type="hidden" name="id" value="<?php echo $id; ?>">
+            <div class="form-group">
+                <label for="title">Title</label>
+                <input type="text"name="title"value="<?php echo$item['title'];?>">
+            <div class="form-group">
+                <label for="content">Berichten</label>
+                <textarea rows="4" cols="50" name="content"><?php echo$item['content'];?></textarea>
+            <div class="form-group">
+                <input type="submit" value="Update">
             
             <!-- Voeg hier de andere velden toe -->
                 
@@ -35,7 +45,7 @@
 
         <!-- Formulier voor delete: -->
         <form action="..." method="POST">
-            <input type="hidden" name="action" value="...">
+            <input type="hidden" name="action" value="delete">
             <!-- Voeg hier nog een veld toe -->
             <input type="submit" value="Verwijder bericht">
         </form>
