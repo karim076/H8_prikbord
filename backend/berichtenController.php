f633ef1d48913ced4eda5508969a4de990c6f6d8
karim076 <59794989+karim076@users.noreply.github.com>
2022-03-12 15:01:45 +0100

    Function: Get id from form and title and content , conecting database putting the new information in database and redirect to the main page

--- backend/berichtenController.php
+++ backend/berichtenController.php
@@ -4,34 +4,45 @@ $action = $_POST['action'];
 if($action == "edit")
 {
     //Haal variabelen op, doe inputvalidatie
+    $id = $_POST['id'];
     $title = $_POST['title'];
-    if(empty($title))
+
+    if (empty($title))
     {
-        die("Vul een titel in")
+        die("Vul een titel in");
     }
 
-    //....
-
-
+    $content = $_POST['content'];
+      
+    if (empty($title))
+    {
+        die("Vul een bericht in");
+    }
 
     //1. Haal de verbinding erbij
-    //...
+    require_once 'conn.php';
 
     //2. Schrijf query met placeholders
-    $query = "UPDATE berichten SET title = :title, content = :content WHERE id = :id";
+    $query = "UPDATE berichten 
+              SET title = :title, content = :content 
+              WHERE id = :id";
     
     //3. Zet query om naar statement
     $statement = $conn->prepare($query);
-
     //4. Voer statement uit, geef nu waarden mee voor de placeholders
-    //...
+    $statement ->execute([
+        ":id" => $id,
+        ":title" => $title,
+        "content" => $content
+    ]);
 
     //5. Niet van toepassing bij een UPDATE-query
 
     //Stuur gebruiker terug naar lijst met berichten (index.php in hoofdmap)
-    //...
+    header("location: http://localhost/Tweede%20Periode/H8_prikbord");
+    die();
 
 }
-
 //.... hier komt de delete-code
+?>
 
